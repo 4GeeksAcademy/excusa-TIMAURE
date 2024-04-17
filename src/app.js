91daa694b9545422b05e9b14af698917eff737f1
TIMAURE <164609178+TIMAURE@users.noreply.github.com>
2024-04-17 10:02:57 +0000

    Js terminado TIMAURE

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function(arr) {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["mi abuelo", "mi perro", "el gato"];
+  let accion = [" no llego a tiempo", "se comio mi tarea", " se comio tu cena"];
+  let donde = ["a buscarme", "en mi casa", "en el portal"];
+
+  function generateExcuse() {
+    let randomquien = quien[Math.floor(Math.random() * quien.length)];
+    let randomaccion = accion[Math.floor(Math.random() * accion.length)];
+    let randomdonde = donde[Math.floor(Math.random() * donde.length)];
+
+    const excuse = `${randomquien} ${randomaccion} ${randomdonde}.`;
+    console.log(document.getElementById("excuse"));
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+  document
+    .getElementById("generateExcuseBtn")
+    .addEventListener("click", generateExcuse);
+  console.log("Generador de excusas inicializado!");
 };
